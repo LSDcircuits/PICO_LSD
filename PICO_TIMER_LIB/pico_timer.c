c631f162cad156c6f655cbf36cb640238cbce370
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2025-07-26 18:56:16 +0200

    Update pico_timer.c

--- PICO_TIMER_LIB/pico_timer.c
+++ PICO_TIMER_LIB/pico_timer.c
@@ -1,7 +1,20 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/timer.h"
 
-#define TL 0x40054024
-#define TH 0x40054028
+#define TL (*(volatile uint32_t*)(0x40054024))
+#define TH (*(volatile uint32_t*)(0x40054028))
+
+
+uint64_t read_timer_raw_macro() {
+    uint32_t hi1, lo, hi2;
+    do {
+        hi1 = timer_hw->timerawh;
+        lo = timer_hw->timerawl;
+        hi2 = timer_hw->timerawh;
+    } while (hi1 != hi2);
+    return ((uint64_t)hi1 << 32) | lo;
+}
 
 //--------------------------------------------------------------
 // unsafe timer for readings under 1 second, this can be useful for very fast sampling
@@ -23,15 +36,11 @@ uint64_t read_timer_LT() {
 
 // gives safe readings doesent allow the timer to overflow
 uint64_t read_timer_raw() {
-    volatile uint32_t* TIMERAWL = (uint32_t*)TL;
-    volatile uint32_t* TIMERAWH = (uint32_t*)TH;
     uint32_t hi1, lo, hi2;
-
     do {
-        hi1 = *TIMERAWH;
-        lo  = *TIMERAWL;
-        hi2 = *TIMERAWH;
+        hi1 = TH;
+        lo  = TL;
+        hi2 = TH;
     } while (hi1 != hi2);
-
     return ((uint64_t)hi1 << 32) | lo;
 }
