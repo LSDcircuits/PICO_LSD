a229a32100645fbf67728d05f8321c53759b14c7
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2025-09-27 08:18:38 +0200

    Update lsd_ultraS.c

--- lsd_ultraS/lsd_ultraS.c
+++ lsd_ultraS/lsd_ultraS.c
@@ -4,34 +4,38 @@
 #include "hardware/clocks.h"
 #include "hardware/adc.h"
 #include "pulse.pio.h"  // side-set based dual-pulse generator
-
+#include "pico_timer.h"
 
 
 int main() {
     stdio_init_all();
-
     // ---- ADC Setup ----
     adc_init();
     adc_gpio_init(26);      // GPIO26 = ADC channel 0
     adc_select_input(0);    // Use ADC0 (GPIO26)
-
     // ---- PIO Pulse Setup ----
     const uint PIN_BASE = 16;    // Using GPIO16 and GPIO17
     const float pio_div = 781;
     PIO pio = pio0;
     uint sm = 0;
     uint offset = pio_add_program(pio, &pulse_program);
-
     while (true){
         pulse_program_init(pio, sm, offset, PIN_BASE, pio_div);
         pio_sm_set_enabled(pio, sm, false);  
         pio_sm_set_enabled(pio, sm, true);
-        sleep_ms(100);
+        uint64_t t0 = read_timer_raw_macro();
         uint16_t raw = adc_read();                    // 0–4095
         uint32_t mv = (raw * 3300) / 4095;            // Convert to mV
+        if (mv < 1) {
+            uint64_t t1 = read_timer_raw_macro();
+            pio_sm_set_enabled(pio, sm, false);  
+            uint64_t dt = t1 - t0;
+            printf("ADC raw: %u, voltage: %u mV\n", raw, mv);
+            printf("Elapsed time: %llu microseconds\n", dt);  // Should be ~100–500 µs
+        }
         printf("ADC raw: %u, voltage: %u mV\n", raw, mv);
         sleep_ms(1);  // Sampling delay
         pio_sm_set_enabled(pio, sm, false);  
         }        // ---- Main Loop ----
-
 }
+
